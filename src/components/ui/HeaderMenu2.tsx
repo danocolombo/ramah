7c87abb52fa3908c74d22c5fd9aebb83aaabe47e
Dano <danocolombo@gmail.com>
2026-05-10 09:15:32 -0400

    media query to minimize menu on smaller devices

--- src/components/ui/HeaderMenu2.tsx
+++ src/components/ui/HeaderMenu2.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 
 interface HeaderMenu2Item {
@@ -22,6 +31,118 @@ export default function HeaderMenu2({
     selectedValue,
     textColor,
 }: HeaderMenu2Props) {
+    const theme = useTheme();
+    const matches = useMediaQuery('(max-width:850px)');
+    const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const [openDrawer, setOpenDrawer] = useState(false);
+
+    const menuTextSx = useMemo(
+        () => ({
+            fontFamily: 'Raleway',
+            fontSize: '1rem',
+            fontWeight: 700,
+            textTransform: 'none',
+        }),
+        [],
+    );
+
+    const drawerItemSx = useMemo(
+        () => ({
+            ...menuTextSx,
+            color: 'white',
+            opacity: 0.7,
+        }),
+        [menuTextSx],
+    );
+
+    const drawerSelectedSx = useMemo(
+        () => ({
+            '&.Mui-selected': {
+                '& .MuiListItemText-root': {
+                    opacity: 1,
+                },
+            },
+        }),
+        [],
+    );
+
+    const drawerItems = useMemo(
+        () => [{ name: 'Home', link: '/', activeIndex: 0 }, ...items],
+        [items],
+    );
+
+    if (matches) {
+        return (
+            <React.Fragment>
+                <SwipeableDrawer
+                    disableBackdropTransition={!iOS}
+                    disableDiscovery={iOS}
+                    open={openDrawer}
+                    onClose={() => setOpenDrawer(false)}
+                    onOpen={() => setOpenDrawer(true)}
+                    slotProps={{
+                        paper: {
+                            sx: { backgroundColor: theme.palette.primary.main },
+                        },
+                    }}
+                >
+                    <Box sx={theme.mixins.toolbar} />
+                    <List disablePadding>
+                        {drawerItems.map((item) => (
+                            <ListItem
+                                key={`${item.link}${item.activeIndex}`}
+                                disablePadding
+                                divider
+                            >
+                                <ListItemButton
+                                    component={Link}
+                                    to={item.link}
+                                    selected={
+                                        selectedValue === item.activeIndex
+                                    }
+                                    sx={drawerSelectedSx}
+                                    onClick={() => {
+                                        setOpenDrawer(false);
+                                        onSelect(item.activeIndex);
+                                    }}
+                                >
+                                    <ListItemText
+                                        sx={drawerItemSx}
+                                        disableTypography
+                                    >
+                                        {item.name}
+                                    </ListItemText>
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </SwipeableDrawer>
+                <IconButton
+                    sx={{
+                        marginLeft: 'auto',
+                        '&:hover': { backgroundColor: 'transparent' },
+                    }}
+                    onClick={() => setOpenDrawer(!openDrawer)}
+                    disableRipple
+                >
+                    <MenuIcon
+                        sx={{
+                            color: textColor,
+                            height: '50px',
+                            width: '50px',
+                            [theme.breakpoints.down('md')]: {
+                                height: '40px',
+                                width: '40px',
+                                marginRight: '1rem',
+                                marginLeft: '1rem',
+                            },
+                        }}
+                    />
+                </IconButton>
+            </React.Fragment>
+        );
+    }
+
     return (
         <Box
             sx={{
@@ -39,7 +160,7 @@ export default function HeaderMenu2({
                     disableRipple
                     onClick={() => onSelect(item.activeIndex)}
                     sx={(theme) => ({
-                        ...theme.typography.tab,
+                        ...menuTextSx,
                         borderBottom:
                             selectedValue === item.activeIndex
                                 ? `2px solid ${textColor}`
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -1,18 +1,10 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 
 import logo from '../../assets/dcLogo.png';
 import HeaderMenu2 from './HeaderMenu2';
@@ -63,30 +55,6 @@ function ElevationScroll(props) {
 
 export default function Header(props) {
     const theme = useTheme();
-    const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-    const matches = useMediaQuery(theme.breakpoints.down('md'));
-
-    const [openDrawer, setOpenDrawer] = useState(false);
-
-    const drawerItemSx = useMemo(
-        () => ({
-            ...theme.typography.tab,
-            color: 'white',
-            opacity: 0.7,
-        }),
-        [theme],
-    );
-
-    const drawerSelectedSx = useMemo(
-        () => ({
-            '&.Mui-selected': {
-                '& .MuiListItemText-root': {
-                    opacity: 1,
-                },
-            },
-        }),
-        [],
-    );
 
     const routes = useMemo(
         () => [
@@ -135,79 +103,6 @@ export default function Header(props) {
         />
     );
 
-    const drawerRoutes = useMemo(
-        () => [
-            { name: 'Home', link: '/', activeIndex: 0 },
-            ...desktopMenuItems,
-        ],
-        [desktopMenuItems],
-    );
-
-    const drawer = (
-        <React.Fragment>
-            <SwipeableDrawer
-                disableBackdropTransition={!iOS}
-                disableDiscovery={iOS}
-                open={openDrawer}
-                onClose={() => setOpenDrawer(false)}
-                onOpen={() => setOpenDrawer(true)}
-                PaperProps={{
-                    sx: { backgroundColor: theme.palette.common.blue },
-                }}
-            >
-                <ToolbarMargin />
-                <List disablePadding>
-                    {drawerRoutes.map((route) => (
-                        <ListItem
-                            key={`${route.link}${route.activeIndex}`}
-                            disablePadding
-                            divider
-                        >
-                            <ListItemButton
-                                component={Link}
-                                to={route.link}
-                                selected={props.value === route.activeIndex}
-                                sx={drawerSelectedSx}
-                                onClick={() => {
-                                    setOpenDrawer(false);
-                                    props.setValue(route.activeIndex);
-                                }}
-                            >
-                                <ListItemText
-                                    sx={drawerItemSx}
-                                    disableTypography
-                                >
-                                    {route.name}
-                                </ListItemText>
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-            </SwipeableDrawer>
-            <IconButton
-                sx={{
-                    marginLeft: 'auto',
-                    '&:hover': { backgroundColor: 'transparent' },
-                }}
-                onClick={() => setOpenDrawer(!openDrawer)}
-                disableRipple
-            >
-                <MenuIcon
-                    sx={{
-                        height: '50px',
-                        width: '50px',
-                        [theme.breakpoints.down('md')]: {
-                            height: '40px',
-                            width: '40px',
-                            marginRight: '1rem',
-                            marginLeft: '1rem',
-                        },
-                    }}
-                />
-            </IconButton>
-        </React.Fragment>
-    );
-
     return (
         <React.Fragment>
             <ElevationScroll>
@@ -224,7 +119,7 @@ export default function Header(props) {
                         >
                             <LogoImage alt='company logo' src={logo} />
                         </LogoLinkButton>
-                        {matches ? drawer : desktopMenu}
+                        {desktopMenu}
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
